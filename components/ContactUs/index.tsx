0607e62c27f489eae4cf21898dd326b5d35618a2
SvirskyiAndrii <a.svirskyi@ghostdrive.com>
2023-03-14 19:34:27 +0200

    Contact us design fixes

--- components/ContactUs/index.tsx
+++ components/ContactUs/index.tsx
@@ -5,17 +5,37 @@ import 'react-phone-number-input/style.css';
 import styles from './styles.module.scss';
 
 export const ContactUs = ({ close }: { close: () => void }) => {
-  const [number, setNumber] = useState();
+  const [number, setNumber] = useState<any>();
+  const [errors, setErrors] = useState<any>(null);
 
   const onFormSubmit = (event: any) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const name = formData.get('name');
-    const email = formData.get('email');
+    const email: any = formData.get('email');
     const company = formData.get('company');
-    console.log({ name, email, number, company });
-    event.target.reset();
-    close();
+    const collectedErrors: any = {};
+
+    if (email?.length > 20) {
+      collectedErrors.email = 'Incorrect email address';
+    }
+    if (number?.length > 20) {
+      collectedErrors.number = 'Incorrect phone number';
+    }
+
+    setErrors(collectedErrors);
+
+    if (!collectedErrors.email && !collectedErrors.number) {
+      console.log({ name, email, number, company });
+      event.target.reset();
+      close();
+    }
+  };
+
+  const clearInputs = () => {
+    if (errors) {
+      setErrors(null);
+    }
   };
 
   return (
@@ -23,28 +43,50 @@ export const ContactUs = ({ close }: { close: () => void }) => {
       <h2 className={styles.title}>Contact Us</h2>
       <div className={styles.inputContainer}>
         <label htmlFor='name'>Name</label>
-        <input name='name' type='text' placeholder='Enter your name' required />
-      </div>
-      <div className={styles.inputContainer}>
-        <label htmlFor='email'>Email</label>
         <input
-          name='email'
-          type='email'
-          placeholder='Enter your email address'
+          name='name'
+          type='text'
+          placeholder='Enter your name'
+          onFocus={clearInputs}
           required
         />
       </div>
       <div className={styles.inputContainer}>
+        <label htmlFor='email'>Email</label>
+        <div className={styles.errors}>
+          <input
+            className={errors?.email ? styles.notValid : ''}
+            name='email'
+            type='email'
+            placeholder='Enter your email address'
+            onFocus={clearInputs}
+            required
+          />
+          {errors?.email && (
+            <span className={styles.error}>{errors.email}</span>
+          )}
+        </div>
+      </div>
+      <div className={styles.inputContainer}>
         <label htmlFor='number'>Phone Number</label>
-        <div className={styles.numberInput}>
+        <div
+          className={`${styles.numberInput} ${
+            errors?.email ? styles.notValid : ''
+          }`}
+        >
           <PhoneInput
+            className={errors?.email ? styles.notValid : ''}
             placeholder='Enter phone number'
             value={number}
             name='number'
             onChange={(e: any) => {
               setNumber(e);
             }}
+            onFocus={clearInputs}
           />
+          {errors?.number && (
+            <span className={styles.error}>{errors.number}</span>
+          )}
         </div>
       </div>
       <div className={styles.inputContainer}>
@@ -53,6 +95,7 @@ export const ContactUs = ({ close }: { close: () => void }) => {
           name='company'
           type='text'
           placeholder='Enter your company name'
+          onFocus={clearInputs}
           required
         />
       </div>
